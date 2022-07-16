d1d9b71c6e8c1d56ac59af91610d12f1da31cf08
Sebastian <sebnow91@gmail.com>
2022-07-16 13:26:52 +0200

    completed help function

--- src/printHelp.c
+++ src/printHelp.c
@@ -1,6 +1,15 @@
 #include "functions.h"
 
 void printHelp(){
-    printf("foo"
-            "bar");
+    printf( "Beli - command line shopping list\n\n"
+            "Usage: ./beli [options] [list]*\n\n"
+            "Options:\n"
+            "   a, add             adds an item to a list\n"
+            "   c, create          creates a new list named [list]\n"
+            "   r, remove          removes a list named [list]\n"
+            "   b, blank           erases whole list named [list]\n"
+            "   p, print           prints [list]'s content\n"
+            "   h, help            prints this help\n\n"
+            "*If no [list] argument is provided the action will be executed on the original list.\n"
+            );
 }
